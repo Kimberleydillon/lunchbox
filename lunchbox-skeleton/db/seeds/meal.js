71682a58519af92eaa4babed2469c24824a2b489
Kimberleydillon <kim.alexandra.dillon@gmail.com>
2016-10-29 20:45:37 +0000

    knex local changes plus node mods installed

---
+++ lunchbox-skeleton/db/seeds/meal.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+ return knex('meal').del()
+    .then(function () {
+      return Promise.all([
+        // Inserts seed entries
+        knex('meal').insert({meal_id: 1, name_meal: 'meaty', cost: 10}),
+        knex('meal').insert({meal_id: 2, name_meal: 'veggies', cost: 10}),
+        knex('meal').insert({meal_id: 3, name_meal: 'non-gluteny', cost: 10})
+      ]);
+    });
+};
