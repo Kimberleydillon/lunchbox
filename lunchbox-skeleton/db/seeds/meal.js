be475d05adcc8ce5f590ea102b56f0c7af3a27b6
Kimberleydillon <kim.alexandra.dillon@gmail.com>
2016-10-30 18:00:46 +0000

    working meal seed and migration

--- lunchbox-skeleton/db/seeds/meal.js
+++ lunchbox-skeleton/db/seeds/meal.js
@@ -7,7 +7,7 @@ exports.seed = function(knex, Promise) {
         // Inserts seed entries
         knex('meal').insert({meal_id: 1, name_meal: 'meaty', cost: 10}),
         knex('meal').insert({meal_id: 2, name_meal: 'veggies', cost: 10}),
-        knex('meal').insert({meal_id: 3, name_meal: 'non-gluteny', cost: 10})
+        knex('meal').insert({meal_id: 3, name_meal: 'gluten Free', cost: 10})
       ]);
     });
 };
--- lunchbox-skeleton/routes/users.js
+++ lunchbox-skeleton/routes/users.js
@@ -5,7 +5,7 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
-  router.get("/", (req, res) => {
+  router.get("/orders", (req, res) => {
     knex
       .select("*")
       .from("meal")
@@ -16,3 +16,22 @@ module.exports = (knex) => {
 
   return router;
 }
+
+
+//app.get("/orders", function(req,res){
+  //to do - GET orders from database
+  var orders = [
+    {
+      name_cust: "linley",
+      phone_num: "+2508935747",
+      meal_id: "meat"
+    },
+    {
+      name_cust: "Nick",
+      phone_num: "+2509208221",
+      meal_id: "veggie"
+    }
+  ]
+
+  res.render("orderConfirm.ejs", { orders: orders })
+});
